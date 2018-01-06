e9c28f90264596e7d9a957fa1846cd5bcb930e3a
guoyunyan <guoyunyanhit@gmail.com>
2018-01-06 10:53:36 +0100

    Correctness in clusters

--- flash.sh
+++ flash.sh
@@ -6,6 +6,6 @@
 #scp mllib/target/spark-mllib_2.11-2.3.0-SNAPSHOT.jar ~/Downloads/spark-2.1.0-bin-hadoop2.6/jars/
 
 ##clusters test
-scp mllib/target/spark-mllib_2.11-2.3.0-SNAPSHOT.jar guoyu@10.111.1.26:/mnt/local/guoyu/
-ssh guoyu@bach16 "mv /mnt/local/guoyu/spark-mllib_2.11-2.3.0-SNAPSHOT.jar /mnt/local/guoyu/spark-2.1.1-bin-hadoop2.7/jars/"
+scp mllib/target/spark-mllib_2.11-2.3.0-SNAPSHOT.jar guoyu@bach03:/mnt/local/guoyu/
+ssh guoyu@bach03 "mv /mnt/local/guoyu/spark-mllib_2.11-2.3.0-SNAPSHOT.jar /mnt/local/guoyu/spark-2.1.1-bin-hadoop2.7/jars/"
 
