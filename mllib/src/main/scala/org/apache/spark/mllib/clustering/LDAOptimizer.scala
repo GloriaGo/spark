62ae5e5bdb5dae1a8ec0b617411035a478a4b381
guoyunyan <guoyunyanhit@gmail.com>
2018-01-18 09:10:37 +0100

    Add decayRate

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/LDAOptimizer.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/LDAOptimizer.scala
@@ -598,8 +598,9 @@ private[clustering] object OnlineLDAOptimizer extends Logging{
                                         deltaLambda: BDM[Double]): (BDM[Double]) = {
 //    logInfo(s"YY=PartitionID:${TaskContext.getPartitionId()}=" +
 //      s"localLambda(2, 2):${localLambda.t.valueAt(2, 2)}\n")
-    val rho = math.pow(tau0 + iteration, -kappa)
-//    val rho = math.pow(tau0 + iteration * decayRate, -kappa)
+//    val rho = math.pow(tau0 + iteration, -kappa)
+    val decayRate = 100
+    val rho = math.pow(tau0 + iteration * decayRate, -kappa)
     val newLambda = (1 - rho) * localLambda + rho * (deltaLambda * corpusSize + eta)
 //    // to garentee the correctness of the change.
 
